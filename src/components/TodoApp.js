4be49e6a88a7614bc81e44d3c0ae54d1fd976948
Ahmad Samir <ahmedsamir96m@gmail.com>
2021-06-30 12:51:52 +0200

    created TodoContainer.js skeleton, imported TodoForm in TodoContainer, Imported TodoContainer in TodoApp

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoContainer from './TodoContainer';
 
 function TodoApp() {
   return (
     <div className="App">
       <h1>Hello Todo!</h1>
+      <TodoContainer />
     </div>
   );
 }
---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoForm from './TodoForm'
+
+function TodoContainer() {
+  return (
+    <div>
+      <TodoForm />
+    </div>
+  )
+}
+
+export default TodoContainer

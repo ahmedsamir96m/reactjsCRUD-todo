c651b966b4ed5eb3c84744048cd3338b0aaa034e
Ahmad Samir <ahmedsamir96m@gmail.com>
2021-06-30 13:14:35 +0200

    Moved the render todos logic, state to TodoContainer, same as the previous commit but in the TodoContainer instead of the TodoApp

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,28 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import TodoContainer from './TodoContainer';
-import TodoItem from './TodoItem';
 
 function TodoApp() {
-  const [todos, setTodos] = useState([{
-    todoTitle: "Todo Title",
-    todoBody: "Todo Body",
-    id: null,
-    isComplete: false
-  }])
 
   return (
     <section className="App">
       <h1>Hello Todo!</h1>
       <TodoContainer />
-      <div className="todo__container">
-        <ul>
-        {todos.map(todo => {
-          return <li key={todo.id}>
-            <TodoItem todo={todo}/>
-          </li>
-        })}
-        </ul>
-      </div>
     </section>
   );
 }
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TodoForm from './TodoForm'
+import TodoItem from './TodoItem';
+
 
 function TodoContainer() {
+  const [todos, setTodos] = useState([{
+    todoTitle: "Todo Title",
+    todoBody: "Todo Body",
+    id: null,
+    isComplete: false
+  }])
+
   return (
     <div>
       <TodoForm />
+      <div className="todo__container">
+        <ul>
+        {todos.map(todo => {
+          return <li key={todo.id}>
+            <TodoItem todo={todo}/>
+          </li>
+        })}
+        </ul>
+      </div>
     </div>
   )
 }

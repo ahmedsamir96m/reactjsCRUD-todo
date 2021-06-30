23f0f16db46045b73ba327099fa8c20f15e4a643
Ahmad Samir <ahmedsamir96m@gmail.com>
2021-06-30 13:46:46 +0200

    created addTodoToTodos function with logic to create new todo and add it to the todos state, passed the function to to TodoForm as prop, used the function the TodoForm on submit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
+  const {addTodoToTodos} = props;
   return (
-    <form className="TodoForm">
+    <form className="TodoForm" onSubmit={addTodoToTodos}>
       <input type="text" name="todo__title" id="todo__title" />
       <textarea name="todo__body" id="todo__body" cols="30" rows="10"></textarea>
       <button type="submit">Add to list</button>

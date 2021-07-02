729114d70a7e381abf806583f64862f868099e32
Ahmad Samir <ahmedsamir96m@gmail.com>
2021-07-02 17:28:17 +0200

    created TodoApp.css, added some styles , imported it to TodoApp.js, added some classes to the other components and elements

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,10 +3,10 @@ import React from 'react'
 function TodoForm(props) {
   const {addTodoToTodos} = props;
   return (
-    <form className="TodoForm" onSubmit={addTodoToTodos}>
-      <input type="text" name="todo__title" id="todo__title" />
-      <textarea name="todo__body" id="todo__body" cols="30" rows="5" ></textarea>
-      <button type="submit">Add to list</button>
+    <form className="todo__Form" onSubmit={addTodoToTodos}>
+      <input className="todo__title" type="text" name="todo__title" id="todo__title" placeholder="Title"/>
+      <textarea className="todo__body" name="todo__body" id="todo__body" cols="30" rows="5" placeholder="Body" ></textarea>
+      <button className="todo__button todo__add" type="submit">Add to list</button>
     </form>
   )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,14 +6,16 @@ function TodoItem(props) {
   const {todo, deleteTodo, editTodo} = props;
   return (
     <div className="todo__item">
-      <h3 title={todo.title}>{todo.todoTitle}</h3>
-      <p title={todo.todoBody}>{todo.todoBody}</p>
-      <button onClick={() => {editTodo(todo.id)}} title="Edit">
-        <img src={editIcon} alt="Update Todo" />
-      </button>
-      <button onClick={() => {deleteTodo(todo.id)}} title="Delete">
-        <img src={deleteIcon} alt="Delete Todo" />
-      </button>
+      <h3 className="todo__item-title" title={todo.title}>{todo.todoTitle}</h3>
+      <p className="todo__item-body" title={todo.todoBody}>{todo.todoBody}</p>
+      <div className="todo__buttons">
+        <button className="todo__button todo__edit" onClick={() => {editTodo(todo.id)}} title="Edit">
+          <img className="todo__icon" src={editIcon} alt="Update Todo" />
+        </button>
+        <button className="todo__button todo__delete" onClick={() => {deleteTodo(todo.id)}} title="Delete">
+          <img className="todo__icon" src={deleteIcon} alt="Delete Todo" />
+        </button>
+      </div>
     </div>
   )
 }

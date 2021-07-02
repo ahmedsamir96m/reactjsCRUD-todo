f27d720d13bb904301d5b5cef557d4735683bf45
Ahmad Samir <ahmedsamir96m@gmail.com>
2021-07-02 16:06:09 +0200

    imported edit,delete icons, adjusted the redered HTML to support this icons instead of plain text

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import editIcon from '../assets/icons/edit-icon.svg'
 
 function TodoItem(props) {
   const {todo, deleteTodo, editTodo} = props;
@@ -6,7 +7,9 @@ function TodoItem(props) {
     <div className="todo__item">
       <h3>{todo.todoTitle}</h3>
       <p>{todo.todoBody}</p>
-      <button onClick={() => {editTodo(todo.id)}}>Update</button>
+      <button onClick={() => {editTodo(todo.id)}}>
+        <img src={editIcon} alt="" />
+      </button>
       <button onClick={() => {deleteTodo(todo.id)}}>Delete</button>
     </div>
   )

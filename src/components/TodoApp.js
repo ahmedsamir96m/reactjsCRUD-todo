a7ea35686a115f98c3e9f8b0a786a1ac987c0a69
Ahmad Samir <ahmedsamir96m@gmail.com>
2021-07-05 15:22:17 +0200

    moved the app header to the TodoContainer

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -7,7 +7,6 @@ function TodoApp() {
 
   return (
     <section className="App">
-      <h1>Check what you have to do today!</h1>
       <TodoContainer />
       <TodoFooter devLinkdin="https://www.linkedin.com/in/ahmedsamir96m/"/>
     </section>
